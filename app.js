991d10ebbb55bdc58fe3f3c510126ecb62ff1991
greeshmanth-dev <greeshmanthsundari@gmai.com>
2023-01-06 23:39:47 +0530

    Here hashing method is used to encrypt

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const ejs=require("ejs");
 const { userInfo } = require("os");
 const mongoose=require("mongoose");
 const { stringify } = require("querystring");
-const encrypt=require("mongoose-encryption");
+//const encrypt=require("mongoose-encryption");
 
+const md5=require("md5");
 const app=express();
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -20,7 +21,7 @@ const userSchema=new mongoose.Schema({
   password:String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
+//userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
 
 const User= new mongoose.model("User",userSchema);
 
@@ -39,7 +40,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -55,7 +56,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
     const email=req.body.username;
-    const pass=req.body.password;
+    const pass=md5(req.body.password);
 
    User.findOne({email:email},function(err,foundUser){
     if(err){
